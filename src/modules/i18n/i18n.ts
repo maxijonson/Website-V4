11f416ffb634f6498ecd9ec6d476ac506108a6ef
MaxiJonson <cody137@hotmail.com>
2019-03-12 19:47:23 -0400

    MJ-12 Implemented i18next markdown

--- src/modules/i18n/i18n.ts
+++ src/modules/i18n/i18n.ts
@@ -1,6 +1,8 @@
 import i18n from "i18next";
+import markdownJsx from "i18next-markdown-jsx-plugin";
 import * as _ from "lodash";
 import { initReactI18next } from "react-i18next";
+
 import enLong from "src/i18n/en/en-long.json";
 import en from "src/i18n/en/en.json";
 import frLong from "src/i18n/fr/fr-long.json";
@@ -9,38 +11,40 @@ import fr from "src/i18n/fr/fr.json";
 const DEFAULT_LNG = "en";
 
 i18n.languages = [DEFAULT_LNG, "fr"];
-i18n.use(initReactI18next).init({
-    load: "languageOnly",
-    resources: {
-        en: {
-            translation: {
-                ...{ long: enLong },
-                ...en,
+i18n.use(markdownJsx)
+    .use(initReactI18next)
+    .init({
+        load: "languageOnly",
+        resources: {
+            en: {
+                translation: {
+                    ...{ long: enLong },
+                    ...en,
+                },
+            },
+            fr: {
+                translation: {
+                    ...en,
+                    ...{ long: enLong },
+                    ...{ long: frLong },
+                    ...fr,
+                },
             },
         },
-        fr: {
-            translation: {
-                ...en,
-                ...{ long: enLong },
-                ...{ long: frLong },
-                ...fr,
-            },
-        },
-    },
-    lng: (() => {
-        for (const lng of i18n.languages) {
-            if (_.includes(navigator.language, lng)) {
-                return lng;
+        lng: (() => {
+            for (const lng of i18n.languages) {
+                if (_.includes(navigator.language, lng)) {
+                    return lng;
+                }
             }
-        }
-        return DEFAULT_LNG;
-    })(),
-    fallbackLng: DEFAULT_LNG,
-});
+            return DEFAULT_LNG;
+        })(),
+        fallbackLng: DEFAULT_LNG,
+    });
 
 export const t = (
     key: string | string[],
-    options?: string | i18n.TOptions<i18n.StringMap> | undefined,
+    options?: i18n.TOptions<i18n.StringMap> | undefined,
 ) => i18n.t(key, options);
 
 export default i18n;
