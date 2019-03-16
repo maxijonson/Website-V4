44a0aca3b0ede2b7290a456e12dd157be1f743ca
MaxiJonson <cody137@hotmail.com>
2019-03-16 00:28:18 -0400

    NCP Mobile disable OverlayScrollbars

--- public/libs/OverlayScrollbars/init.js
+++ public/libs/OverlayScrollbars/init.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     //The first argument are the elements to which the plugin shall be initialized
     //The second argument has to be at least a empty object or a object with your desired options
-    OverlayScrollbars(document.querySelectorAll("body"), {});
+    if (screen && screen.width > 600) {
+        OverlayScrollbars(document.querySelectorAll("body"), {});
+    }
 });
