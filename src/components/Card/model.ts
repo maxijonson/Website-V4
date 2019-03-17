acdf54a520a9e8819baadd79415d05f967d3189c
MaxiJonson <cody137@hotmail.com>
2019-03-17 13:21:28 -0400

    Card renderer can return null

--- src/components/Card/model.ts
+++ src/components/Card/model.ts
@@ -1,7 +1,7 @@
 import { ITheme } from "src/modules/CSS";
 
 export type IChildren = React.ReactNode;
-export type IRenderer = (props: { children?: IChildren }) => JSX.Element;
+export type IRenderer = (props: { children?: IChildren }) => JSX.Element | null;
 export type IBodyAlignment = "left" | "right";
 
 export interface ISCProps {
