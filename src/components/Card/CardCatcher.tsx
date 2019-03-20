e41d16c6cc55052fd6cedd009ef1a68087e3117a
MaxiJonson <cody137@hotmail.com>
2019-03-19 22:40:04 -0400

    MJ-34 Modal more flexible on container

--- src/components/Card/CardCatcher.tsx
+++ src/components/Card/CardCatcher.tsx
@@ -75,13 +75,35 @@ const Base = connect(mapStateToProps)(
                 <Modal
                     visible={modalVisible}
                     onRequestClose={onModalRequestClose}
-                    ContainerRenderer={({ children }) => (
-                        <Card cardClassName="cardCatcher--modal--card">
-                            {children}
-                        </Card>
-                    )}
                 >
-                    Hello
+                    <Card
+                        cardClassName="cardCatcher--modal--card"
+                        title="title"
+                        subtitle="sub"
+                    >
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit. Voluptate praesentium molestiae ea quia
+                        accusantium dolores tempora recusandae nobis aspernatur
+                        facere incidunt, est aliquid id porro consectetur optio
+                        odit ratione ipsam. Lorem ipsum dolor sit amet,
+                        consectetur adipisicing elit. Accusamus recusandae
+                        consequuntur perspiciatis iure, fugiat nostrum et id
+                        explicabo! Cupiditate voluptatibus fugit blanditiis
+                        magni tenetur impedit dolorem ullam repudiandae delectus
+                        ea. Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veritatis omnis qui eius consequatur odit modi non
+                        dolorem mollitia nesciunt nemo eligendi veniam,
+                        recusandae sed eveniet? Iure dolore consequuntur
+                        expedita sint. Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Voluptas enim eveniet ad provident
+                        odio excepturi autem similique aperiam nisi rerum, vero
+                        cum esse, magnam suscipit doloremque culpa? Magnam, ab
+                        nam! Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Error eveniet eum delectus magnam ipsum sequi
+                        possimus unde veritatis quae facere blanditiis quod
+                        repellat officia sunt commodi voluptatum est, porro
+                        fugit.
+                    </Card>
                 </Modal>
             </>
         );
