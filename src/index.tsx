c2a016a16cd5478cf030d2125bfa602e8879437d
MaxiJonson <cody137@hotmail.com>
2019-03-20 19:25:19 -0400

    MJ-25 Menu icon

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faCopyright } from "@fortawesome/free-regular-svg-icons";
-import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 import "normalize.css/normalize.css"; // Reset stylesheet for cross-browser compatibility
 import * as React from "react";
 import * as ReactDOM from "react-dom";
@@ -12,7 +12,7 @@ import { APP_ROOT } from "./config";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/config";
 
-library.add(faMoon, faSun, faCopyright);
+library.add(faMoon, faSun, faCopyright, faBars);
 
 const store = configureStore();
 
