b1ea1b0a39871a619b8feead25165484bb5a11d8
MaxiJonson <cody137@hotmail.com>
2019-03-27 18:32:00 -0400

    Removed commented out code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,28 +3,28 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import { THEME_TRANSITION_TIME } from "src/config/config";
 import { Hooks } from "src/modules";
-import styled from "styled-components";
+import { useStyled } from "src/modules/hooks/hooks";
 import { fonts } from "../modules/CSS";
 
 const { useMapState } = Hooks;
-
-const Footer = styled.footer`
-    color: ${({ theme }) => theme.colors.defaultText};
-    font-size: 1.2rem;
-    text-align: center;
-    padding-bottom: 1%;
-    transition: all ${THEME_TRANSITION_TIME}s;
-    background-color: ${({ theme }) => theme.colors.pageBackground};
-    font-family: ${fonts.oswald.family};
-    width: 100%;
-`;
 
 export default () => {
     const { theme } = useMapState(({ theme }) => ({ theme }));
     const { t } = useTranslation();
 
+    const Footer = useStyled.footer(`
+        color: ${theme.colors.defaultText};
+        font-size: 1.2rem;
+        text-align: center;
+        padding-bottom: 1%;
+        transition: all ${THEME_TRANSITION_TIME}s;
+        background-color: ${theme.colors.pageBackground};
+        font-family: ${fonts.oswald.family};
+        width: 100%;
+    `);
+
     return (
-        <Footer theme={theme}>
+        <Footer>
             Copyright <FontAwesomeIcon icon={["far", "copyright"]} />{" "}
             {new Date().getFullYear()} MaxiJonson. {t("footer.copyright")}
         </Footer>
