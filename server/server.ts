09b2e9e97730810ae98e29bf1d9739a263591b81
MaxiJonson <cody137@hotmail.com>
2019-04-17 20:10:11 -0400

    MJ-52 Secure requests over https

--- server/server.ts
+++ server/server.ts
@@ -3,9 +3,17 @@ import path from "path";
 
 const app = express();
 const publicPath = path.join(__dirname, "../public");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.static(publicPath));
+
+// Redirect HTTP to HTTPS
+app.use((req, res, next) => {
+    if (req.hostname !== "localhost" && !req.secure) {
+        return res.redirect(`https://${req.hostname}${req.url}`);
+    }
+    return next();
+});
 
 app.get("*", (_req, res) => {
     res.sendFile(path.join(publicPath, "index.html"));
